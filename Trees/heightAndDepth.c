3ab3163bbfed3a3c83fe8ba34d3e37aff25a2627
Sairam S <125436937+S-Sairam@users.noreply.github.com>
2024-12-11 19:57:14 +0530

    Create heightAndDepth.c

---
+++ Trees/heightAndDepth.c
@@ -0,0 +1,22 @@
+int height(struct Node* root) {   // height is also same as maximum depth
+    if (root == NULL) {
+        return 0;
+    }
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+    return (leftHeight > rightHeight ? leftHeight : rightHeight) + 1;
+}
+
+
+int depth(struct Node* root, int key) { // depth of a node always requires a key
+    if (root == NULL) return -1;
+    if (root->data == key) return 0;
+    
+    int leftDepth = depth(root->left, key);
+    if (leftDepth != -1) return leftDepth + 1;
+    
+    int rightDepth = depth(root->right, key);
+    if (rightDepth != -1) return rightDepth + 1;
+    
+    return -1; // Node not found
+}
